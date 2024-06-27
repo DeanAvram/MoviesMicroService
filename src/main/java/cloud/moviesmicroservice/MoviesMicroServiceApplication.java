2ba837a1e09ce7d8f8bccd0f747fd5c24be8e377
DeanAvram <dean6198@gmail.com>
2024-06-27 12:28:39 +0300

    Initial Commit

---
+++ src/main/java/cloud/moviesmicroservice/MoviesMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package cloud.moviesmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesMicroServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cloud/moviesmicroservice/MoviesMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.moviesmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
